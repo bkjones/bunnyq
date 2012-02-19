4b40f3a1574c2e948fb073c902814200349c9986
Brian K. Jones <bkjones@gmail.com>
2012-02-19 10:31:27 -0500

    Added ability to list exchanges, and to list bindings for a queue. Put groundwork in place for better arg parsing and less code duplication. More on the way.

--- bunny.py
+++ bunny.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
-from amqplib import client_0_8 as amqp
 import cmd
-import readline
+import functools
 import getpass
-from pyrabbit import api
+from operator import methodcaller
+import sys
+from pyrabbit import api, http
 
 # prefer 'input' in 2.7 (req'd in 3.2). Fall back to raw_input for 2.6.
 input = getattr(__builtins__, 'input', None) or raw_input
+
+def parse_keyval_args(func):
+    @functools.wraps(func)
+    def wrapper(inst, args):
+        try:
+            d = dict(arg.split('=') for arg in args.split())
+        except ValueError:
+            # there were probably spaces around the '='
+            raise ValueError("Invalid input: try removing spaces from around "
+                             "the '=' sign in your argument(s)")
+        else:
+            try:
+                return func(inst, **d)
+            except TypeError as out:
+                print(out)
+    return wrapper
+
+
 
 class Bunny(cmd.Cmd):
     """Represents a session between a client and a RabbitMQ server, so you can
@@ -17,8 +36,8 @@ class Bunny(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt = "--> "
-        self.qlist = {"/": []}
         self.host = None
+        self.port = None
         self.user = None
         self.password = None
         self.vhost = '/'
@@ -26,12 +45,15 @@ class Bunny(cmd.Cmd):
     def do_connect(self, line):
         if not self.host:
             self.host = raw_input("Host: ")
+            self.port = raw_input("Port: ")
             self.user = raw_input("Username: ")
             self.password = getpass.getpass()
 
         try:
             print("Connecting to %s as %s" % (self.host, self.user))
-            self.srv = api.Server(self.host, self.user, self.password)
+            self.srv = api.Client('%s:%s' % (self.host, self.port),
+                                  self.user,
+                                  self.password)
             print("Success!")
             #connection/channel creation success, change prompt
             self.prompt = "%s.%s: " % (self.host, self.vhost)
@@ -39,8 +61,31 @@ class Bunny(cmd.Cmd):
             print("Connection or channel creation failed")
             print("Error was: ", out)
 
+    def request(self, call, help=self.do_help):
+        request = methodcaller(call)
+        try:
+            val = request(self.srv)
+        except api.PermissionError:
+            whoami = self.srv.get_whoami()
+            print("You don't have sufficient permissions to access the user"
+                  " listing. Login info: %s" % repr(whoami))
+            return
+        except (ValueError, IOError, http.HTTPError) as out:
+            print(repr(type(out)), out)
+            help()
+        except Exception as out:
+            print(repr(type(out)), out)
+        else:
+            return val
+
     def do_list_users(self, name):
-        users = self.srv.get_users()
+        try:
+            users = self.srv.get_users()
+        except api.PermissionError:
+            whoami = self.srv.get_whoami()
+            print("You don't have sufficient permissions to access the user"
+                  " listing. Login info: %s" % repr(whoami))
+            return
         for user in users:
             u = "Name: {name}\nAdmin: {administrator}\n".format(**user)
             print u
@@ -55,15 +100,17 @@ class Bunny(cmd.Cmd):
             if not name:
                 raise ValueError("You need provide a name for the queue")
             else:
-                self.srv.queue_declare(name)
-                self.qlist["/"].append(name)
+                self.srv.create_queue(name, self.vhost)
         except ValueError as out:
-            print(out)
+            print(repr(type(out)), out)
             self.help_create_queue()
         except IOError as out:
-            print(out)
+            print(repr(type(out)), out)
+        except http.HTTPError as out:
+            print out
+            return
         except Exception as out:
-            print(out)
+            print(repr(type(out)), out)
             self.help_create_queue()
 
     def help_create_queue(self):
@@ -77,7 +124,7 @@ class Bunny(cmd.Cmd):
             if not name or name is None:
                 self.help_purge_queue()
             else:
-                msgcount = self.srv.queue_purge(name, nowait=False)
+                msgcount = self.srv.purge_queue(self.vhost, name)
                 print("Purged %i messages\n" % msgcount)
         except Exception as out:
             print(out)
@@ -89,18 +136,32 @@ class Bunny(cmd.Cmd):
 
     def do_qlist(self, s):
         print("\n")
-        for exch, queues in self.qlist.iteritems():
-            print("Exchange: %s" % exch)
-            for q in queues:
-                print("\t%s" % q)
-        print("\n")
+        out_fmt = "{0:<20}|{1:<20}|{2:<20}|{3:<20}"
+        cell_line = ('-'*20+'+')*4
+        hdr_fields = ["Vhost", "Queue", "Consumers", "Idle Since"]
+        hdr = out_fmt.format(*hdr_fields)
+        print(hdr)
+        print(cell_line)
+        for vhost in self.srv.get_all_vhosts():
+            vname = vhost['name']
+            queues = self.srv.get_queues(vname)
+            if not queues:
+                print("\tVhost %s has no queues!" % vname)
+                print(cell_line)
+            else:
+                for queue in queues:
+                    idle_since = queue['idle_since']
+                    consumers = queue['consumers']
+                    print(out_fmt.format(vname, queue['name'],
+                                     consumers, idle_since))
+                    print(cell_line)
+
+    def do_queue_detail(self, args):
+        pass
 
     def do_delete_queue(self, name):
         try:
-            self.srv.queue_delete(name)
-            for k, v in self.qlist.iteritems():
-                if name in v:
-                    v.remove(name)
+            self.srv.delete_queue(self.vhost, name)
         except IOError as out:
             print(out)
         except Exception as out:
@@ -111,39 +172,39 @@ class Bunny(cmd.Cmd):
         print("\n".join(["\tdelete_queue <qname>",
                          "\tDeletes the named queue."]))
 
-    def do_create_exchange(self, args):
-        name = None
-        d = self.parseargs(args)
-        type = d.get('type') or 'direct'
-        print("TYPE: %s" % type)
-
-        if 'name' not in d:
+    @parse_keyval_args
+    def do_create_exchange(self, name, vhost='/', type='direct'):
+        if not name:
             print("You must provide a name!")
             self.help_create_exchange()
-        else:
-            name = d['name']
-
         try:
-            self.srv.exchange_declare(name, type)
-            self.qlist[name] = []
+            self.srv.create_exchange(vhost, name, type)
         except IOError as out:
             print(out)
         except Exception as out:
-            print(out)
-            print("Invalid input. Here's some help: ")
-            self.help_create_exchange()
+            raise
 
     def help_create_exchange(self):
         print("\n".join(["\tcreate_exchange name=<name> [type=<type>]",
                          "\tCreate an exchange with given name. Type is 'direct' by default."]))
 
+    def do_xlist(self, s):
+        print("\n")
+        for vhost in self.srv.get_all_vhosts():
+            vname = vhost['name']
+            print("Vhost: %s" % vname)
+            exchanges = self.srv.get_exchanges(vname)
+            if not exchanges:
+                print("\t-- no exchanges! --\n")
+            else:
+                for exchange in exchanges:
+                    print("\t'%s'" % exchange['name'])
+                print("\n")
+        print('\n')
+
     def do_delete_exchange(self, name):
         try:
-            self.srv.exchange_delete(name)
-            if self.qlist.has_key(name):
-                for q in self.qlist[name]:
-                    self.qlist["/"].append(q)
-                del self.qlist[name]
+            self.srv.delete_exchange(self.vhost, name)
         except IOError as out:
             print(out)
         except Exception as out:
@@ -154,12 +215,10 @@ class Bunny(cmd.Cmd):
         print("\n".join(["\tdelete_exchange <exchange>",
                          "\tDeletes the named exchange."]))
 
-    def do_create_binding(self, bstring):
+    @parse_keyval_args
+    def do_create_binding(self, queue, exchange):
         try:
-            d = self.parseargs(bstring)
-            queue = d['queue']
-            exchange = d['exchange']
-            self.srv.queue_bind(queue, exchange)
+            self.srv.create_binding(self.vhost, queue, exchange)
         except Exception as out:
             print("Invalid input. Here's some help: ")
             self.help_create_binding()
@@ -169,11 +228,47 @@ class Bunny(cmd.Cmd):
         print("\n".join(["\tcreate_binding exchange=<exch> queue=<queue>",
                          "\tBinds given queue to named exchange"]))
 
+    @parse_keyval_args
+    def do_list_queue_bindings(self, queue, vhost):
+        """
+        Get a listing of all bindings for a named queue.
+
+        :param str qname: Name of the queue to list bindings for
+        :param str vhost: Vhost the qname lives in.
+
+        """
+        vhost = '%2F' if vhost is '/' else vhost
+        try:
+            bindings = self.srv.get_queue_bindings(vhost, queue)
+        except http.HTTPError as out:
+            print(out)
+            return
+
+        if bindings:
+            out_fmt = "{0:<20}|{1:<20}|{2:<20}|{3:<20}|{4:<20}"
+            cell_line = ('-'*20+'+')*5
+            hdrs = ["Vhost", "Source Exch", "Dest Queue",
+                    "Routing Key", "Arguments"]
+            hdr = out_fmt.format(*hdrs)
+            print(hdr)
+            print(cell_line)
+            for binding in bindings:
+                src_exch = binding['source'] or 'AMQP Default'
+                rt_key = binding['routing_key']
+                arguments = binding['arguments']
+                out_vhost = '/' if vhost is '%2F' else vhost
+                line = out_fmt.format(out_vhost, src_exch, queue,
+                                       rt_key, arguments)
+                print(line)
+
+    def help_list_queue_bindings(self):
+        print("\n".join(["\tlist_queue_bindings queue=<queue> vhost=<vhost>",
+                        "\tList binding details for the named queue"]))
+
     def do_send_message(self, args):
         try:
             exchange, message_txt = args.split(':')
-            message = amqp.Message(message_txt)
-            self.srv.basic_publish(message, exchange)
+            self.srv.publish(self.vhost, exchange, None, message_txt)
         except Exception as out:
             print(out)
             self.help_send_message()
@@ -186,7 +281,7 @@ class Bunny(cmd.Cmd):
     def do_dump_message(self, qname):
         """This only does a basic_get right now. You can't specify a particular message."""
         try:
-            msg = self.srv.basic_get(qname, no_ack=True)
+            msg = self.srv.get_messages(self.vhost, qname)
             if msg is not None:
                 print(msg.body)
             else:
@@ -202,10 +297,8 @@ class Bunny(cmd.Cmd):
 
     def do_get_status(self, qname):
         try:
-            q, msgcount, consumers = self.srv.queue_declare(queue=qname,
-                                                             passive=True)
-            print("%s: %d messages, %d consumers" % (q, msgcount, consumers))
-
+            q_properties = self.srv.get_queue(self.vhost, qname)
+            print q_properties
         except Exception as out:
             print(out)
             print(self.help_get_status())
@@ -218,6 +311,7 @@ class Bunny(cmd.Cmd):
         pass
 
     def do_EOF(self, line):
+        print("\n")
         return True
 
     def do_exit(self):
@@ -228,5 +322,9 @@ class Bunny(cmd.Cmd):
         return d
 
 
-shell = Bunny()
-shell.cmdloop()
+if __name__ == '__main__':
+    shell = Bunny()
+    if len(sys.argv) > 1:
+        shell.onecmd(' '.join(sys.argv[1:]))
+    else:
+        shell.cmdloop()
